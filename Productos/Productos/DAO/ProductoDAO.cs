0b141ca8e67d72bb710055768caf17c70a73c46b
Andres Perez <156430410+Andrezzzz421@users.noreply.github.com>
2025-08-15 18:42:22 +0200

    Add files via upload

---
+++ Productos/Productos/DAO/ProductoDAO.cs
@@ -0,0 +1,71 @@
+﻿using Productos.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos.DAO
+{
+    public class ProductoDAO
+    {
+        public static List<Producto> productos = new List<Producto>();
+
+        public List<Producto> GetAll()
+        {
+            return productos;
+        }
+
+        public Producto GetById(int id)
+        {
+            foreach (var p in productos)
+            {
+                if (p.Id == id) return p;
+            }
+            return null;
+        }
+
+        public void Add(Producto prd)
+        {
+            var pr = GetById(prd.Id);
+            if (pr != null)
+            {
+                productos.Add(pr);
+            }
+            else
+            {
+                pr.Nombre = prd.Nombre;
+                pr.Cantidad = prd.Cantidad;
+                pr.PrecioCompra = prd.PrecioCompra;
+                pr.PrecioVenta = prd.PrecioVenta;
+                pr.Categoria = prd.Categoria;
+                pr.Disponible = prd.Disponible;
+                pr.Foto = prd.Foto;
+            }
+        }
+
+        public void Update(Producto prd)
+        {
+            var pr = GetById(prd.Id);
+            if (pr != null)
+            {
+                pr.Nombre = prd.Nombre;
+                pr.Cantidad = prd.Cantidad;
+                pr.PrecioCompra = prd.PrecioCompra;
+                pr.PrecioVenta = prd.PrecioVenta;
+                pr.Categoria = prd.Categoria;
+                pr.Disponible = prd.Disponible;
+                pr.Foto = prd.Foto;
+            }
+        
+        }
+        
+        public void Delete(int id)
+        {
+            var pr = GetById(id);
+            if (pr != null)
+            {
+                productos.Remove(pr);
+        }   }
+    }
+}
---
+++ Productos/Productos/UI/FrmProductos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Productos.UI
+{
+    public partial class FrmProductos : Form
+    {
+        public FrmProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmProductos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
