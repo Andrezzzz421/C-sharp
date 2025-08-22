2fb484de2ca81c9c03317a2c490be1b05419ca32
Andres Perez <156430410+Andrezzzz421@users.noreply.github.com>
2025-08-21 20:40:31 -0500

    Add files via upload

---
+++ Productos/Productos/HELPERS/HELPproductos.cs
@@ -0,0 +1,97 @@
+﻿using Productos.DAO;
+using Productos.DTO;
+using Productos.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos.HELPERS
+{
+    public class HELPproductos
+    {
+        private static ProductoDAO dao = new ProductoDAO();
+        private static CategoriaDAO cdao = new CategoriaDAO();
+
+        public static void Seed()
+        {
+            Producto ob1 = new Producto()
+            {
+                Id = 1,
+                Nombre = "Papa",
+                Cantidad = 100,
+                PrecioCompra = 1000,
+                PrecioVenta = 1500,
+                Categoria = 1,
+                Disponible = true,
+                Foto =null
+            };
+            Producto ob2 = new Producto()
+            {
+                Id = 2,
+                Nombre = "Cebolla",
+                Cantidad = 100,
+                PrecioCompra = 1000,
+                PrecioVenta = 1500,
+                Categoria = 1,
+                Disponible = true,
+                Foto = null
+           
+            };
+            dao.Add(ob1);
+            dao.Add(ob2);
+
+           
+        }
+
+        public static ProductoDTO ToDTO(int id)
+        {
+            var x = dao.GetById(id);
+            if(x != null)
+            {
+                string nombre = cdao.GetById(x.Categoria).Nombre;
+                return new ProductoDTO()
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre,
+                    Cantidad = x.Cantidad,
+                    PrecioVenta = x.PrecioVenta,
+                    NombreCategoria = nombre,
+                    Foto = x.Foto != null ? HELPproductos.toImage(x.Foto): null
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static byte[] ToBytes(string ruta)
+        {
+            using (Image image = Image.FromFile(ruta))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    return ms.ToArray();
+                }
+
+            }
+        }
+
+        public static Image toImage(byte[] b)
+        {
+            using (MemoryStream ms = new MemoryStream(b))
+            {
+                Image image = Image.FromStream(ms);
+                return Image.FromStream(ms);
+            }
+        }
+
+
+    }
+}
---
+++ Productos/Productos/UI/FrmMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Productos.UI
+{
+    public partial class FrmMenu : Form
+    {
+        private Form formularioActivo = null;
+        private void AbrirFormularioEnPanel(Form formularioHijo)
+        {
+            if (formularioActivo != null)
+                formularioActivo.Close(); // Cierra el que ya estaba abierto
+
+            formularioActivo = formularioHijo;
+
+            formularioHijo.TopLevel = false; // No será ventana independiente
+            formularioHijo.FormBorderStyle = FormBorderStyle.None; // Sin bordes
+            formularioHijo.Dock = DockStyle.Fill; // Que ocupe todo el panel
+
+            pnlApp.Controls.Clear(); // Limpia el panel
+            pnlApp.Controls.Add(formularioHijo); // Agrega el nuevo formulario
+            //pnlApp.Tag = formularioHijo; // Opcional
+            formularioHijo.Show();
+        }
+
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlApp_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AbrirFormularioEnPanel(new FrmProductos());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AbrirFormularioEnPanel(new FrmCategoria());
+        }
+    }
+}
